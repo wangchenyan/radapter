3b100ae2fd25e6a19c0b4cef784a711f2d2caef6
石工 <chenyan.wcy@koubei.com>
2018-12-30 17:46:30 +0800

    update

--- simple/src/main/java/me/wcy/radapter/simple/MainActivity.kt
+++ simple/src/main/java/me/wcy/radapter/simple/MainActivity.kt
@@ -11,8 +11,8 @@ import me.wcy.radapter.RViewHolderWrap
 import me.wcy.radapter.simple.model.Image
 import me.wcy.radapter.simple.model.Text
 import me.wcy.radapter.simple.viewholder.ImageViewHolder
-import me.wcy.radapter.simple.viewholder.TextViewHolder1
 import me.wcy.radapter.simple.viewholder.TextViewHolder2
+import me.wcy.radapter.simple.viewholder.TextViewHolder1
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,17 +22,17 @@ class MainActivity : AppCompatActivity() {
 
         val dataList = mutableListOf<Any>()
         dataList.add(Image(R.mipmap.image1))
-        dataList.add(Text("渊虹", 1))
+        dataList.add(Text("渊虹", 2))
         dataList.add(Image(R.mipmap.image2))
-        dataList.add(Text("鲨齿", 2))
+        dataList.add(Text("鲨齿", 1))
         dataList.add(Image(R.mipmap.image3))
-        dataList.add(Text("干将莫邪", 1))
+        dataList.add(Text("干将莫邪", 2))
         dataList.add(Image(R.mipmap.image4))
-        dataList.add(Text("墨眉", 2))
+        dataList.add(Text("墨眉", 1))
         dataList.add(Image(R.mipmap.image5))
-        dataList.add(Text("水寒", 1))
+        dataList.add(Text("水寒", 2))
         dataList.add(Image(R.mipmap.image6))
-        dataList.add(Text("太阿", 2))
+        dataList.add(Text("太阿", 1))
 
         val adapter = RAdapter(dataList)
         adapter.register(Image::class.java, ImageViewHolder::class.java)
