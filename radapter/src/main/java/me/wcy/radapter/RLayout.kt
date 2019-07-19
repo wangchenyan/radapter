b8f742531193e7568e71cbeabef23595442527b7
石工 <chenyan.wcy@koubei.com>
2019-07-19 16:17:36 +0800

    update

--- radapter/src/main/java/me/wcy/radapter/RLayout.kt
+++ radapter/src/main/java/me/wcy/radapter/RLayout.kt
@@ -1,12 +1,10 @@
 package me.wcy.radapter
 
 import android.support.annotation.LayoutRes
-import java.lang.annotation.Retention
-import java.lang.annotation.RetentionPolicy
 
 /**
  * ViewHolder 设置布局文件的注解
  */
-@Target(AnnotationTarget.CLASS, AnnotationTarget.FILE)
-@Retention(RetentionPolicy.RUNTIME)
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
 annotation class RLayout(@LayoutRes val value: Int)
